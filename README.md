934959ee54a45f312d7c6b292f3831fd962f3bd6
Sam Littlefair <sam.littlefair@prismic.io>
2021-01-21 13:24:34 +0100

    Add custom types, add default repo to config, update readme

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 # nuxt-prismic-boilerplate
 
-A basic boilerplate for Nuxt and Prismic.
+A basic boilerplate for Nuxt and Prismic. This boilerplate does not currently use Slice Machine, but it will be added in the future.
 
 ## Features
 
-- SCSS
-- SEO
+- Configured with SCSS.
+- Has an SEO helper to generate `title`, `meta`, `og`, and `twitter` tags.
+- Comes with a `config` and a `page` custom type with basic meta fields. It should be possible to create a repo with these files with the `theme` command in the Prismic CLI, or by copy-pasting them into a repo.
+- `config` custom type includes an "Emoji Favicon" field, to set the site's favicon.
+- Link Resolver
+- HTML Serializer that handles Nuxt links
+- One dynamic route (work in progress)
+- One root route (work in progress)
 
 ## To Do
 
+- [x] Emoji favicon
+- [ ] Finish dynamic route
+- [ ] Finish root route
 - [ ] Head templates
 - [ ] Custom types
 - [ ] SliceZone
 - [ ] Layout file
-- [x] Emoji
+- [ ] Map custom types
 - [ ] Imgix image component?
+- [ ] Map custom type fields?
 
 ## Noteworthy Files
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,41 +1,34 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Loading...',
+    title: "Loading...",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    "~/plugins/seo-handler.js"
-  ],
+  plugins: ["~/plugins/seo-handler.js"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    '@nuxtjs/prismic'
-  ],
+  buildModules: ["@nuxtjs/prismic"],
 
   // Settings for prismic.
   prismic: {
-    endpoint: 'https://sam-onboarding-nuxt-blog.cdn.prismic.io/api/v2'
+    endpoint: "https://nuxt-blrplt.cdn.prismic.io/api/v2",
   },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {},
+};
