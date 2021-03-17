3892e34db477909eb8d28a4df61399b070d284a4
Sam Littlefair <sam.littlefair@prismic.io>
2021-03-17 10:29:44 +0100

    Scope layout styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 .layout-default-outer {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
     Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
